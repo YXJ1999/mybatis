24818ea592a593fb25ac5099e3ee626e44968b2f
YXJ1999 <996832282@qq.com>
2021-12-14 18:01:18 +0800

    在公司提交

--- src/main/java/com/yang/mybatis/dao/EmployeeMapper.java
+++ src/main/java/com/yang/mybatis/dao/EmployeeMapper.java
@@ -1,7 +1,24 @@
 package com.yang.mybatis.dao;
 
 import com.yang.mybatis.bean.Employee;
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 public interface EmployeeMapper {
     public Employee getEmployeeById(Integer id);
+    public void add(Employee employee);
+    public void update(Employee employee);
+    public void delete(Integer id);
+    public Employee getEmpByIdAndLastName(@Param("id") String id, @Param("lastName")String lastName);
+    public Employee getEmpByMap(Map map);
+    public List<Employee> getEmployeeByLastNameLike(String lastName);
+    //返回一条记录的map:key就是列名，值就是对应的值
+    public Map<String,Object> getEmpByIdReturnMap(Integer id);
+    //多条记录封装一个map:Map<Integer,Employee> :键是记录的主键，值是封装后的Java对象
+    //告诉mybatis封装这个map的时候使用哪一个属性作为key
+    @MapKey("id")
+    public Map<Integer,Employee> getEmpByLastNameLikeReturnMap(String lastName);
 }
--- src/main/java/com/yang/mybatis/test/MybatisTest.java
+++ src/main/java/com/yang/mybatis/test/MybatisTest.java
@@ -12,14 +12,17 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MybatisTest {
-    public SqlSession getSqlSession() throws IOException {
+    public SqlSessionFactory getSqlSessionFactory() throws IOException {
         String resource="mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        return sqlSession;
+        return sqlSessionFactory;
     }
     /**
      * 1.根据配置文件（全局配置文件）创建一个sqlSessionFactory对象
@@ -83,6 +86,45 @@ public class MybatisTest {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         EmployeeMapperAnnotation mapper = sqlSession.getMapper(EmployeeMapperAnnotation.class);
         System.out.println(mapper.getEmployeeById(1));
+        sqlSession.close();
+    }
+    @Test
+    public void test2 () throws Exception{
+        SqlSessionFactory sqlSessionFactory=getSqlSessionFactory();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+//        Employee dack = new Employee(null, "dack", "2@qq.com", "1");
+//        mapper.add(dack);
+//        System.out.println(dack);
+//        mapper.update(new Employee(1,"tom","1@qq.com","0"));
+//        mapper.delete(5);
+        Employee tom = mapper.getEmpByIdAndLastName("1", "tom");
+        System.out.println(tom);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public void test3()throws Exception{
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+//        HashMap<Object, Object> hashMap = new HashMap<>();
+//        hashMap.put("id",1);
+//        hashMap.put("lastName","tom");
+//        Employee empByMap = mapper.getEmpByMap(hashMap);
+//        System.out.println(empByMap);
+        List<Employee> employeeByLastNameLike = mapper.getEmployeeByLastNameLike("%o%");
+        for (Employee e:employeeByLastNameLike) {System.out.println(e);}
+        sqlSession.close();
+    }
+    @Test
+    public void test4() throws Exception{
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+//        Map<String, Object> map = mapper.getEmpByIdReturnMap(1);
+        Map<Integer, Employee> map = mapper.getEmpByLastNameLikeReturnMap("tom");
+        System.out.println(map);
         sqlSession.close();
     }
 }
